3d23059ac5eb45bda1963bd71e8a8d4a2d350c93
Vikasswami1 <Vikas.Swami@mindtree.com>
2022-06-30 13:26:02 +0530

    moving modified file from new1

--- Dropdowns.java
+++ Dropdowns.java
@@ -11,11 +11,14 @@ public class Dropdowns {
 		// TODO Auto-generated method stub
 		WebDriver driver=new ChromeDriver();
 		System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe" );
+                driver.getTitle();
 		driver.get("https://spicejet.com/");
 		Select s=new Select(driver.findElement(By.id("")));
 		s.selectByValue("6");
 		s.selectByIndex(1);
 		s.selectByVisibleText("adult2");
+
+
 		//for static dropdowns with select in html code
 
 	}
